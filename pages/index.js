1af334200e939b4409d47f6b706d1c2136eec888
Azeke Work <azeke.work@gmail.com>
2022-06-30 16:58:29 +0600

    validation system 1

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
 			<main className={styles.main}>
         <Toolbar>
-          <h1>SENDING FORM </h1>
+          <h1>PAYMENT FORM </h1>
         </Toolbar>
 				<FormComponent />
 			</main>
--- src/FormComponent.jsx
+++ src/FormComponent.jsx
@@ -1,85 +1,144 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { useEffect, useState } from "react";
+import style from './FormComponent.module.css'
+import InputMask from "react-input-mask";
 import {
-    Container,
-    TextField,
-    Button,
-  } from "@material-ui/core";
+  Container,
+  TextField,
+  Button,
+} from "@material-ui/core";
 
 const FormComponent = () => {
-    const { register, handleSubmit, formState: {errors} } = useForm();
-    const onSubmit = (data) => console.log(data);
-    function cc_format(value) {
-        var v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '')
-        var matches = v.match(/\d{4,16}/g);
-        var match = matches && matches[0] || ''
-        var parts = []
-    
-        for (i=0, len=match.length; i<len; i+=4) {
-            parts.push(match.substring(i, i+4))
-        }
-    
-        if (parts.length) {
-            return parts.join(' ')
-        } else {
-            return value
-        }
+  const [ccNumber, setCcNumber] = useState('');
+  const [expDate, setExpDate] = useState('');
+  const [CVV, setCVV] = useState('');
+  const [amount, setAmount] = useState('');
+
+  const [cardNumberError, setCardNumberError] = useState(false);
+  const [expDateError, setExpDateError] = useState(false);
+  const [cvvError, setCvvNumberError] = useState(false);
+  const [amountError, setAmountError] = useState(false);
+
+  const validate = () => {
+    if (ccNumber.length == 0 || ccNumber == undefined) {
+      setCardNumberError(true);
+    }else{
+      setCardNumberError(false);
     }
+  }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setCardNumberError(false);
+    setExpDateError(false);
+    setCvvNumberError(false);
+    setAmountError(false);
 
-    return (<>
-    <Container maxWidth ="xs">
-    <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField
-          style={{ margin: "5px" }}
-          id = "cardNumber"
-          type="text"
-          label="Card Number"
-          variant="outlined"
-          fullWidth
-          {...register("cardNumber", {required: "Field is required ", pattern: {
-            value: /^(?:4[0-9]{12}(?:[0-9]{3})?)$/,
-            message: "Invalid card number"
-          }})}
-          error = {!!errors?.cardNumber}
-          helperText = {errors?.cardNumber ? errors.cardNumber.message : null}
-        />
+    if (ccNumber === '') {
+      setCardNumberError(true);
+    }
+    if (expDate === '') {
+      setExpDateError(true);
+    }
+    if (CVV === '') {
+      setCvvNumberError(true);
+    }
+    if (amount === '') {
+      setAmountError(true);
+    }
+  }
+
+  return (<>
+    <Container maxWidth="xs">
+      <form onSubmit={handleSubmit}>
+        <div>Card number:</div>
+        <InputMask
+          mask="9999 9999 9999 9999"
+          value={ccNumber}
+          disabled={false}
+          onChange={(e) => {setCcNumber(e.target.value); validate()}}
+        >
+          {() => <TextField
+            required
+            fullWidth
+            style={{ margin: "5px" }}
+            variant="outlined"
+            placeholder="1111 2222 3333 4444"
+            error={cardNumberError}
+            helperText={ccNumber === "" ? "Empty field! Please enter 16 digits" : " "}
+          />}
+        </InputMask>
         <br />
-        {/* <TextField
-          style={{ width: "200px", margin: "5px" }}
-          id = "expDate"
-          required
-          type="text"
-          label="Expiration Date"
-          variant="outlined"
-        />
+        <div className={style.expDate}>
+          <div>
+            <div>Expiration Date:</div>
+            <InputMask
+              mask="99/9999"
+              value={expDate}
+              disabled={false}
+              onChange={(e) => {setExpDate(e.target.value); validate()}}
+            >
+              {() => <TextField
+                required
+                style={{ width: "200px", margin: "5px" }}
+                variant="outlined"
+                placeholder="MM/YYYY"
+                error={expDateError}
+                helperText={expDate === "" ? "Empty field!" : " "}
+              />}
+            </InputMask>
+          </div>
+          <div>
+            <div className={style.title}>CVV:</div>
+            <InputMask
+              mask="999"
+              value={CVV}
+              disabled={false}
+              onChange={(e) => {setCVV(e.target.value); validate()}}
+            >
+              {() => <TextField
+                required
+                style={{ width: "55px", margin: "5px 15px" }}
+                variant="outlined"
+                placeholder="123"
+                error={cvvError}
+                helperText={CVV === "" ? "Required!" : " "}
+              />}
+            </InputMask>
+          </div>
+
+        </div>
+        <div>Amount:</div>
+        <InputMask
+          mask = "99999999999999"
+          maskChar=" "
+          value={amount}
+          disabled={false}
+          onChange={(e) => {setAmount(e.target.value); validate()}}
+        >
+          {() => <TextField
+            required
+            fullWidth
+            style={{ margin: "0px 5px" }}
+            variant="outlined"
+            error={amountError}
+            helperText={amount === "" ? "Empty field!" : " "}
+          />}
+        </InputMask>
         <br />
-        <TextField
-          style={{ width: "100px", margin: "5px" }}
-          id = "cvv"
-          required
-          type="text"
-          label="CVV"
-          variant="outlined"
-        />
-        <br />
-        <TextField
-          style={{ margin: "5px" }}
-          id = "amount"
-          required
-          type="text"
-          label="Amount"
-          variant="outlined"
-          fullWidth
-        />
-        <br /> */}
-        <Button type = "submit" variant="contained" color="primary" fullWidth style={{ margin: "5px" }} >
+        <Button 
+          type="submit" 
+          variant="contained" 
+          color="primary" 
+          fullWidth 
+          style={{ margin: "5px" }} 
+          disabled={cardNumberError}
+        >
           send
         </Button>
       </form>
     </Container>
-    
-    </>)
+
+  </>)
 }
 
 export default FormComponent;
